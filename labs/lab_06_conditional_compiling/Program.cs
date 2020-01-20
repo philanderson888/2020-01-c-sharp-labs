59c42f0c94c75fed9275b9702b90cb6b18866f89
Phil Anderson <philanderson888@hotmail.com>
2020-01-20 16:29:09 +0000

    adding wpf lab

---
+++ labs/lab_06_conditional_compiling/Program.cs
@@ -0,0 +1,25 @@
+﻿#define THISISTRUE
+using System;
+
+namespace lab_06_conditional_compiling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is always printed");
+
+#if DEBUG
+            Console.WriteLine("This is debug code");
+#else
+            Console.WriteLine("This is production code");
+#endif
+
+#if THISISTRUE
+            Console.WriteLine("This is true");
+#endif
+
+
+        }
+    }
+}

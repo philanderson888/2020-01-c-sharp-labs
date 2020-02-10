dab77835d9c537fb52f71b7d8ab3a2fe8528994a
Phil Anderson <philanderson888@hotmail.com>
2020-02-10 16:44:30 +0000

    Updating 2020-01-c-sharp-labs

---
+++ labs/lab_46_asp_demo_site/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace lab_46_asp_demo_site.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ labs/lab_47_mvc_framework_demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab_47_mvc_framework_demo.MvcApplication" Language="C#" %>

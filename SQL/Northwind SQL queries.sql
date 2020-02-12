e7f72fcf448eaf0d5aa40b2c76a3d6bf33443248
Phil Anderson <philanderson888@hotmail.com>
2020-02-12 17:40:57 +0000

    Updating 2020-01-c-sharp-labs

--- SQL/Northwind SQL queries.sql
+++ SQL/Northwind SQL queries.sql
@@ -1,4 +1,7 @@
-﻿select top 5 * from customers
+﻿use northwind
+go
+
+select top 5 * from customers
 select top 5 * from customers order by customerid desc
 
 -- quiz : find count of employees in London
@@ -152,3 +155,216 @@ join orders o on c.CustomerID = o.CustomerID
 join [Order Details] od on o.OrderID = od.OrderID
 join products p on od.productid = p.productid
 where c.customerid = 'alfki'
+
+-- now can add some CALCULATED COLUMNS eg ORDER BEFORE DISCOUNT 
+select c.customerid, contactname, o.OrderID, p.ProductID, ProductName, od.Quantity , od.UnitPrice , 
+od.Discount * 100  as 'Percentage Discount' ,
+od.UnitPrice * od.Quantity as 'Order Before Discount', 
+od.UnitPrice * od.Quantity * od.Discount as 'Discount',
+od.UnitPrice * od.Quantity - od.UnitPrice * od.Quantity * od.Discount as 'Order After Discount',
+od.UnitPrice * od.Quantity * (1 - od.Discount) as 'order after discount'
+from  customers c
+join orders o on c.CustomerID = o.CustomerID
+join [Order Details] od on o.OrderID = od.OrderID
+join products p on od.productid = p.productid
+where c.customerid = 'alfki'
+
+-- can we get totals  (HAVE TO OMIT THE OTHER FIELDS FOR NOW)
+select  
+sum (od.Quantity) as 'quantity total', 
+sum (od.UnitPrice * od.Quantity) as 'Order Before Discount', 
+sum (od.UnitPrice * od.Quantity * od.Discount) as 'Discount',
+sum (od.UnitPrice * od.Quantity * (1 - od.Discount)) as 'order after discount'
+from  customers c
+join orders o on c.CustomerID = o.CustomerID
+join [Order Details] od on o.OrderID = od.OrderID
+join products p on od.productid = p.productid
+where c.customerid = 'alfki'
+
+-- Now we have this working for ONE CUSTOMER ALKFI.
+-- How can we get this working for ALL CUSTOMERS???
+-- can use a GROUP BY condition which groups results by a column
+-- SO WE CAN REPEAT THESE TOTALS AND GROUP BY CUSTOMERID
+select  
+c.customerid,
+sum (od.Quantity) as 'quantity total', 
+sum (od.UnitPrice * od.Quantity) as 'Order Before Discount', 
+sum (od.UnitPrice * od.Quantity * od.Discount) as 'Discount',
+sum (od.UnitPrice * od.Quantity * (1 - od.Discount)) as 'order after discount'
+from  customers c
+join orders o on c.CustomerID = o.CustomerID
+join [Order Details] od on o.OrderID = od.OrderID
+join products p on od.productid = p.productid
+group by c.customerid
+
+-- sort results by biggest spender!
+select  
+c.customerid,
+sum (od.Quantity) as 'quantity total', 
+sum (od.UnitPrice * od.Quantity) as 'Order Before Discount', 
+sum (od.UnitPrice * od.Quantity * od.Discount) as 'Discount',
+sum (od.UnitPrice * od.Quantity * (1 - od.Discount)) as 'order after discount'
+from  customers c
+join orders o on c.CustomerID = o.CustomerID
+join [Order Details] od on o.OrderID = od.OrderID
+join products p on od.productid = p.productid
+group by c.customerid
+order by [order after discount] desc
+
+-- filter where discount is not zero  (WHERE)
+-- also only show orders where total order after discount > 10000 (HAVING)
+select  
+c.customerid,
+sum (od.Quantity) as 'quantity total', 
+sum (od.UnitPrice * od.Quantity) as 'Order Before Discount', 
+sum (od.UnitPrice * od.Quantity * od.Discount) as 'Discount Price',
+sum (od.UnitPrice * od.Quantity * (1 - od.Discount)) as 'order after discount'
+from  customers c
+join orders o on c.CustomerID = o.CustomerID
+join [Order Details] od on o.OrderID = od.OrderID
+join products p on od.productid = p.productid
+where od.Discount > 0
+
+group by c.customerid
+having sum (od.UnitPrice * od.Quantity * (1 - od.Discount)) > 10000
+
+order by [order after discount] desc
+
+
+
+-- ### STRING MANIUPLATION
+
+-- SUBSTRING from first given index (1-based counting) and specify number of letters to return
+SELECT SUBSTRING('verylongstring',3,6)  -- from index 3 (3rd character) for 6 letters -- rylong
+
+-- CHARINDEX -- get the index of character where it is placed inside long string
+--              NOTE AGAIN INDEX IS NOT ZERO BASED COUNTING SO RETURS 6 = 6TH CHARACTER
+SELECT CHARINDEX ('a','verylangstrang')
+SELECT CHARINDEX (' ','TM44 5AZ')  -- SPLIT UP A POSTCODE USING THIS
+
+-- LEFT/RIGHT  RETURN FIRST / LAST n characters
+SELECT LEFT('verylongstring',4)  -- very
+SELECT LEFT('TM44 5AZ',CHARINDEX (' ','TM44 5AZ')-1) -- should select TM44
+SELECT RIGHT('TM44 5AZ', 3) -- should select 5AZ
+SELECT RIGHT('TM44 5AZ', LEN('TM44 5AZ')-CHARINDEX (' ','TM44 5AZ')) -- should select 5AZ
+
+-- LTRIM/RTRIM removes spaces
+SELECT LTRIM(RTRIM(('   some data with space around   ')))
+
+-- REPLACE
+SELECT REPLACE('very long string','very','very very very extremely')
+
+-- UPPER/LOWER
+SELECT UPPER ('a long string in lower case')
+
+
+
+-- DATES
+
+-- TODAY
+SELECT GetDate()
+SELECT CONCAT('Today is ', GETDATE())
+SELECT CONCAT('Tomorrow is ',DATEADD(d,1,GETDATE()))
+SELECT CONCAT('Yesterday was ',DATEADD(d,-1,GETDATE()))
+-- DATE DIFF  eg difference in days between today and a week's time
+SELECT DATEDIFF(d, DATEADD(d,7,GETDATE()) ,  GETDATE()  )
+
+-- d   or   dd    or  mm   or  yy  or   yyyy 
+
+SELECT * FROM ORDERS 
+
+-- WHAT'S THE LONGEST SHIPPING TIME???  SUBTRACT SHIPDATE FROM ORDERDATE AND GET MAX!
+SELECT ORDERID, DATEDIFF(D, ORDERDATE, SHIPPEDDATE) AS SHIPPINGTIME
+FROM ORDERS O
+ORDER BY SHIPPINGTIME DESC
+
+-- FLOOR - CONVERT TO INTEGER
+select floor(10.65)
+SELECT CEILING(10.65)
+SELECT ROUND(10.49,0)
+SELECT ROUND(10.50,0)
+
+-- CASE .. WHEN .. ELSE
+
+SELECT CASE
+WHEN (10<11)
+THEN '10 is small'
+ELSE 'some other number'
+END
+AS 'is 10 small?'
+
+SELECT * FROM ORDERS 
+
+
+-- GROUP BY EXERCISES
+
+--- PRINT CUSTOMERS GROUPED BY COUNTRY?
+SELECT Count(Country),Country 
+FROM Customers 
+GROUP BY COUNTRY 
+
+-- SUM THE COUNT
+SELECT COUNT(COUNTRY) FROM CUSTOMERS GROUP BY COUNTRY
+
+SELECT Country, ContactName
+FROM Customers 
+ORDER BY COUNTRY 
+
+--- PRINT CUSTOMERS GROUPED BY CITY?
+SELECT CITY, CONTACTNAME FROM CUSTOMERS ORDER BY CITY 
+
+-- PRINT PRODUCTS GROUPED BY CATEGORY?
+SELECT  COUNT(p.ProductName), c.CATEGORYNAME
+FROM PRODUCTS p
+INNER JOIN CATEGORIES c ON p.CategoryId = c.CategoryId
+GROUP BY c.CategoryName
+
+select CategoryName , c.CategoryID , ProductName 
+FROM PRODUCTS p
+INNER JOIN CATEGORIES c on p.CategoryID = c.CategoryID
+ORDER BY C.CategoryName
+
+-- all products
+select * from products order by categoryid
+
+
+
+--- LEFT, RIGHT, INNER, OUTER JOINS 
+
+--- LEFT (OUTER) JOIN : ALL RECORDS FROM LEFT TABLE AND MATCHING FROM RIGHT TABLE
+
+		-- ALL CUSTOMERS (BY CUSTOMERID) AND ANY MATCHING ORDERS
+
+--- RIGHT (OUTER) JOIN : ALL ORDERS (BY ORDERID) AND ANY MATCHING CUSTOMERS 
+
+--- OUTER JOIN = EITHER LEFT (OUTER) JOIN OR RIGHT (OUTER) JOIN WHICH ARE DESCRIBED ABOVE
+
+--- INNER JOIN = ONLY FROM FIRST TABLE WHEN MATCHING SECOND TABLE
+
+		-- ONLY CUSTOMERS WHO HAVE ORDERS, AND THEIR MATCHING ORDER
+
+-- can we do an INNER JOIN and compare with STANDARD LEFT JOIN?  DO ALL CUSTOMERS HAVE ORDERS?
+SELECT CustomerId 
+FROM Customers c
+
+
+SELECT COUNT(*) FROM CUSTOMERS
+SELECT COUNT(*) FROM ORDERS 
+SELECT c.CustomerId, o.OrderID
+FROM Customers c 
+LEFT JOIN Orders o on c.CustomerID = o.CustomerID
+WHERE o.OrderID IS NULL
+ORDER BY OrderID
+
+-- NOW WE CAN CHECK LEFT JOIN VS INNER JOIN SHOULD DIFFER BY 2 RECORDS
+SELECT COUNT(*)
+FROM CUSTOMERS c
+LEFT JOIN Orders o on c.CustomerID = o.CustomerID
+SELECT COUNT(*)
+FROM CUSTOMERS c
+INNER JOIN Orders o on c.CustomerID = o.CustomerID
+SELECT COUNT(*)
+FROM CUSTOMERS c
+FULL JOIN Orders o on c.CustomerID = o.CustomerID
+
+
---
+++ labs/lab_49_async_await/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+
+namespace lab_49_async_await
+{
+    class Program
+    {
+
+        static List<string> fileRows = new List<string>();
+        static void Main(string[] args)
+        {
+
+
+            // standard code is SYNCHRONOUS IE LINE BY LINE, THREAD IS ONLY ONE THREAD 
+            // AND IT 'HANGS' if you have long operations
+            var s = new Stopwatch();
+            s.Start();
+            Console.WriteLine($"Starting stopwatch at time {s.ElapsedMilliseconds}");
+
+            DoThis(); // sync 
+            Console.WriteLine(s.ElapsedMilliseconds);
+
+            // async operation
+            // main thread will start the operation but not wait for it.  Main thread will not pause.
+            // build a big text file
+            File.Delete("log.dat");
+            Console.WriteLine("\n\nBuilding text file\n\n");
+            for (int i = 0; i < 1000; i++)
+            {
+                File.AppendAllText("log.dat", $"New log entry {i} at {DateTime.Now}\n");
+            }
+            Console.WriteLine("File built . . . \n\n");
+            Console.WriteLine($"Starting async operations at {s.ElapsedMilliseconds}");
+            ReadTextLines();  // async
+            Console.WriteLine($"Finishing async operations at {s.ElapsedMilliseconds}");
+            Console.WriteLine("Program has completed");
+        }
+
+        static void DoThis()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Finished doing this");
+        }
+
+        static async void ReadTextLines()
+        {
+            var array = await File.ReadAllLinesAsync("log.dat");
+            fileRows.AddRange(array); // add array to list
+            // print sample ie every 1000 lines
+
+            var counter = 0;
+            foreach(var item in fileRows)
+            {
+                Console.WriteLine(item);
+                counter++;
+                if (counter > 100) break;
+            }
+        }
+    }
+}

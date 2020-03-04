41655ab4138a8549b9e733053451391188f61dd6
Phil Anderson <philanderson888@hotmail.com>
2020-03-04 18:21:54 +0000

    API and MVC

---
+++ labs/lab_61_MVC_Website_Cats/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using lab_60_cats_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab_61_MVC_Website_Cats
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // at project startup, create a live instance of our database
+            // and make it available to whole application
+            // don't have to call using (var db = new CatDbConext()){} each time any more
+            // INJECTING DATABASE INTO OUR APPLICATION
+            // DATABASE IS NOW A SERVICE
+
+            services.AddDbContext<CatDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("CatDb")));
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}

a9a1283f8ee9058d8ffbbcf0b059d6c9d7b7c28e
Phil Anderson <philanderson888@hotmail.com>
2020-03-09 16:42:01 +0000

    Adding Azure, Kubernetes and Worker

---
+++ labs/WorkerService1/Worker.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WorkerService1
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogWarning("Philip Anderson Test Service Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(60000, stoppingToken);
+            }
+        }
+    }
+}
